17f0c677e78bd70d931b2946fef7680fed903dde
lenkly <lenk.lydia@gmail.com>
2020-04-13 17:13:58 +0200

    Add bright and dark theme

---
+++ client/src/themes/brightmode.js
@@ -0,0 +1,11 @@
+const brightmode = {
+  colors: {
+    text: 'rgba(0, 0, 0, 1)',
+    texthighlight: 'rgba(187, 0, 255, 1)',
+    background: 'rgba(249, 249, 249, 1)',
+    backgroundSignin: 'rgba(249, 249, 249, 0.97)',
+    backgroundLogin: 'rgba(230, 230, 230, 0.97)',
+  },
+};
+
+export default brightmode;
---
+++ client/src/themes/darkmode.js
@@ -0,0 +1,11 @@
+const darkmode = {
+  colors: {
+    text: 'rgba(255, 255, 255, 1)',
+    texthighlight: 'rgba(255, 247, 0, 1)',
+    background: 'rgba(29, 29, 29, 1)',
+    backgroundSignin: 'rgba(29, 29, 29, 0.97)',
+    backgroundLogin: 'rgba(62, 62, 62, 0.97)',
+  },
+};
+
+export default darkmode;
