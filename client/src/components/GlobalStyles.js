b750d6a27c99a8dae15c9e411a83bebc7d7727b7
lenkly <lenk.lydia@gmail.com>
2020-04-13 16:56:56 +0200

    Add globalstyles

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { Global } from '@emotion/core';
+import { Global, css } from '@emotion/core';
+//import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Rubik&display=swap');
 
 function GlobalStyles() {
-  return <Global></Global>;
+  return (
+    <Global
+      styles={(theme) => css`
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+        }
+        body {
+          height: 100vh;
+          font-size: 34;
+          font-family: 'Quicksand', sans-serif;
+          color: ${theme.color.text};
+          background: ${theme.colors.background};
+          margin: 0;
+        }
+      `}
+    />
+  );
 }
 export default GlobalStyles;
