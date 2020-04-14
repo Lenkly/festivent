393d10e74642e7f867e6099d351d897bbd79f96d
lenkly <lenk.lydia@gmail.com>
2020-04-14 14:57:08 +0200

    Change color codes on themes

--- client/src/themes/brightmode.js
+++ client/src/themes/brightmode.js
@@ -1,8 +1,8 @@
 const brightmode = {
   colors: {
-    text: 'rgba(0, 0, 0, 1)',
-    texthighlight: 'rgba(187, 0, 255, 1)',
-    background: 'rgba(249, 249, 249, 1)',
+    text: '#000',
+    texthighlight: '#bb00ff',
+    background: '#f9f9f9',
     backgroundSignin: 'rgba(249, 249, 249, 0.97)',
     backgroundLogin: 'rgba(230, 230, 230, 0.97)',
   },
--- client/src/themes/darkmode.js
+++ client/src/themes/darkmode.js
@@ -1,8 +1,8 @@
 const darkmode = {
   colors: {
-    text: 'rgba(255, 255, 255, 1)',
-    texthighlight: 'rgba(255, 247, 0, 1)',
-    background: 'rgba(29, 29, 29, 1)',
+    text: '#fff',
+    texthighlight: '#fff700',
+    background: '#1d1d1d',
     backgroundSignin: 'rgba(29, 29, 29, 0.97)',
     backgroundLogin: 'rgba(62, 62, 62, 0.97)',
   },
