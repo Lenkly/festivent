f3e3faccf4cb32e9a36b442a54511709a9038a5d
lenkly <lenk.lydia@gmail.com>
2020-04-14 15:28:29 +0200

    Set theming colors to globalstyles

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
-//import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Rubik&display=swap');
+import QuicksandLight from '../assets/QuicksandLight.otf';
+import QuicksandBook from '../assets/QuicksandBook.otf';
+import Rubik from '../assets/Rubik.ttf';
+import themes from 'emotion-theming';
 
 function GlobalStyles() {
   return (
     <Global
-      styles={(theme) => css`
+      styles={css`
         *,
         *:before,
         *:after {
           box-sizing: border-box;
         }
+        @font-face {
+            font-family: 'Quicksand Light';
+            src: url('${QuicksandLight}') format('opentype');
+          }
+          @font-face {
+            font-family: 'Quicksand Book';
+            src: url('${QuicksandBook}') format('opentype');
+          }
+          @font-face {
+            font-family: 'Rubik';
+            src: url('${Rubik}') format('truetype');
+          }
         body {
           height: 100vh;
-          font-size: 34;
-          font-family: 'Quicksand', sans-serif;
-          color: ${theme.color.text};
-          background: ${theme.colors.background};
+          color: ${themes.colors.text};
+          background: ${themes.colors.background};
           margin: 0;
         }
       `}
