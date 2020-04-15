ac5a2cf0f531c762681a5cae2c32ae15a3809954
lenkly <lenk.lydia@gmail.com>
2020-04-15 23:41:18 +0200

    Set default Profile icon

--- client/src/stories/Profileicon.stories.js
+++ client/src/stories/Profileicon.stories.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ProfileIcon from '../components/Profileicon';
+import Icon from '../assets/festivent-grey.svg';
 
 export default {
   title: 'Profile Icon',
 };
 
-export const Usericon = () => <ProfileIcon size="Profile" />;
+export const Usericon = () => <ProfileIcon size="Profile" src={Icon} />;
 
-export const Navigationicon = () => <ProfileIcon size="Nav" />;
+export const Navigationicon = () => <ProfileIcon size="Nav" src={Icon} />;
