24bb90086caa938251f5c5dd6f91958837537aca
lenkly <lenk.lydia@gmail.com>
2020-04-17 15:32:18 +0200

    Fix view compatibility

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -8,38 +8,25 @@ const Button = styled.button`
   width: ${(props) => buttonSize[props.size].width};
   text-transform: uppercase;
   color: #000;
-  background: #f9f9f9;
-  background-clip: padding-box;
-  border: none;
+  background-image: linear-gradient(#f9f9f9, #f9f9f9), linear-gradient(
+    to right,
+    #fff700,
+    #ff9100,
+    #ff0084,
+    #ff00f2,
+    #bb00ff,
+    #7700ff,
+    #006aff,
+    #00ccff,
+    #00ff6a,
+    #59ff00,
+    #fff700
+  );
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  border: double 2px transparent;
   border-radius: 1rem;
-  position: relative;
-
-  &:before {
-    content: '';
-    display: block;
-    position: absolute;
-    width: ${(props) => buttonSize[props.size].borderWidth};
-    height: ${(props) => buttonSize[props.size].height};
-    top: 50%;
-    transform: translateY(-50%);
-    left: -0.5%;
-    z-index: -1;
-    margin: 0;
-    border-radius: 1rem;
-    background: linear-gradient(
-      to right,
-      #fff700,
-      #ff9100,
-      #ff0084,
-      #ff00f2,
-      #bb00ff,
-      #7700ff,
-      #006aff,
-      #00ccff,
-      #00ff6a,
-      #59ff00,
-      #fff700
-    );
+  height: ${(props) => buttonSize[props.size].height};
   }
 `;
 
