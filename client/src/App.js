f6de95fe9f7a05f0a5b2ccb90bf44a475b0be4ce
lenkly <lenk.lydia@gmail.com>
2020-04-20 15:19:50 +0200

    Add route to genre page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import Welcome from './pages/Welcome';
+import Genres from './pages/Genres';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Welcome />
+          </Route>
+          <Route path="/Genres">
+            <Genres />
           </Route>
         </Switch>
       </Router>
