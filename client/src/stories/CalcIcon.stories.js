f7375f14d5fdb0dada766e3432b2bdec0b4a7580
lenkly <lenk.lydia@gmail.com>
2020-04-24 16:06:30 +0200

    Add buttons colores based on calculation value

---
+++ client/src/stories/CalcIcon.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CalcIcon from '../components/CalcIcon';
+
+export default {
+  title: 'Calculation Icon',
+};
+
+export const Perfect = () => <CalcIcon color="perfect">98.6</CalcIcon>;
+
+export const VeryHigh = () => <CalcIcon color="veryhigh">89.4</CalcIcon>;
+
+export const High = () => <CalcIcon color="high">76.2</CalcIcon>;
+
+export const Moderate = () => <CalcIcon color="moderate">70.1</CalcIcon>;
+
+export const Average = () => <CalcIcon color="average">57.5</CalcIcon>;
+
+export const Okay = () => <CalcIcon color="okay">53.8</CalcIcon>;
+
+export const Substandard = () => <CalcIcon color="substandard">41.3</CalcIcon>;
+
+export const Low = () => <CalcIcon color="low">34.7</CalcIcon>;
+
+export const VeryLow = () => <CalcIcon color="verylow">22.4</CalcIcon>;
+
+export const AlmostNone = () => <CalcIcon color="almostnone">5.0</CalcIcon>;
