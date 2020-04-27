034f0142534cfd4a92b0acd4691c830bc4eb56f0
lenkly <lenk.lydia@gmail.com>
2020-04-27 14:03:55 +0200

    Build Genrebutton

---
+++ client/src/components/GenreButton.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+const GenreButton = styled.button`
+  width: 8.3rem;
+  height: 2.75rem;
+  border: none;
+  color: #000;
+  font-family: 'Quicksand Book', sans-serif;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  background: transparent;
+  border-radius: 1rem;
+  padding-top: 5px;
+  margin-right: 10px;
+  margin-top: 10px;
+  box-shadow: 0 0 0px 50px #f9f9f9;
+`;
+export default GenreButton;
+
+// &::active: {
+//     transition: 0.25s linear;
+//     color: #000;
+//     opacity: 100%;
+//     background: none;
+//   }
