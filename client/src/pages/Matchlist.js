011cae39af5a6b3116ed7865d5895b0f47450879
lenkly <lenk.lydia@gmail.com>
2020-04-29 22:45:30 +0200

    Add users name and match again button

--- client/src/pages/Matchlist.js
+++ client/src/pages/Matchlist.js
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 import Content from '../components/layout/Content';
 import Form from '../components/layout/Form';
 import FestivalMatch from '../components/FestivalMatch';
+import Button from '../components/Button';
+import { useHistory } from 'react-router-dom';
 
 const Text = styled.div`
   display: flex;
@@ -13,15 +15,40 @@ const Text = styled.div`
   margin-bottom: 25px;
 `;
 
+const AgainText = styled.div`
+  display: flex;
+  line-height: 1.3;
+  flex-wrap: wrap;
+  font-family: 'Quicksand Light';
+  font-size: 25px;
+  text-align: center;
+  margin-bottom: 25px;
+`;
+
 function Matchlist() {
+  const history = useHistory();
+
+  const nameUser = sessionStorage.getItem('Name') || '[Name]';
+
+  const handleAgainClick = () => {
+    sessionStorage.removeItem('Genres');
+    history.push('/Genres');
+  };
+
   return (
     <Content>
       <Form>
         <Text>
-          [Name],
+          {nameUser},
           <br /> These are your matches
         </Text>
         <FestivalMatch />
+        <AgainText>
+          Still Haven&apos;t Found What You Were Looking For?
+        </AgainText>
+        <Button onClick={handleAgainClick} size="Medium">
+          Match Me Again
+        </Button>
       </Form>
     </Content>
   );
