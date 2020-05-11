e44fbf43513d95b8d422e40a3c313e5f1017b426
Lenkly <lenk.lydia@gmail.com>
2020-05-11 22:27:48 +0200

    Fix design problems

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,10 +1,42 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const disabledStyle = css`
+  background: none;
+  color: #707070;
+  border: solid 2px transparent;
+  cursor: not-allowed;
+`;
+
+const enabledStyle = (props) => css`
+  background-image: linear-gradient(
+      ${props.theme.colors.background},
+      ${props.theme.colors.background}
+    ),
+    linear-gradient(
+      to right,
+      #fff700,
+      #ff9100,
+      #ff0084,
+      #ff00f2,
+      #bb00ff,
+      #7700ff,
+      #006aff,
+      #00ccff,
+      #00ff6a,
+      #59ff00,
+      #fff700
+    );
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  color: ${props.theme.colors.text};
+  border: double 2px transparent;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
 
 const Button = styled.button`
-  ${(props) =>
-    props.disabled
-      ? 'background: none; color: #707070; border: solid 2px #1d1d1d; cursor: not-allowed;'
-      : 'background-image: linear-gradient(#1d1d1d, #1d1d1d), linear-gradient(to right, #fff700, #ff9100, #ff0084, #ff00f2, #bb00ff, #7700ff, #006aff, #00ccff, #00ff6a, #59ff00, #fff700); background-origin: border-box; background-clip: padding-box, border-box; color: #fff; border: double 2px transparent; border-radius: 1rem; cursor: pointer;'}
+  ${(props) => (props.disabled ? disabledStyle : enabledStyle(props))};
   font-family: 'Quicksand Book', sans-serif;
   padding: ${(props) => buttonSize[props.size].padding};
   font-size: ${(props) => buttonSize[props.size].fontsize};
--- client/src/components/SettingsInput.js
+++ client/src/components/SettingsInput.js
@@ -3,6 +3,7 @@ import Input from './Input';
 
 const SettingsInput = styled(Input)`
   display: flex;
+  width: 100%;
   text-align: left;
   &::placeholder {
     text-align: left;
