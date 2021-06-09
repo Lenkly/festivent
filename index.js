ac38166d858de1a64a6c45aac1b6197947174c5e
Lenkly <lenk.lydia@gmail.com>
2021-06-09 22:46:37 +0200

    add image upload routes

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+require('dotenv').config();
 const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const Datauri = require('datauri');
+
 const middlewares = jsonServer.defaults({
   static: path.join(__dirname, 'client/build'),
 });
+
+const upload = multer();
+
+function toDataUri(file) {
+  const dataUri = new Datauri();
+  return dataUri.format(path.extname(file.originalname).toString(), file.buffer)
+    .content;
+}
+
 const port = process.env.PORT || 8080;
+
+server.post(
+  '/api/uploads',
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const dataUriFile = toDataUri(request.file);
+      console.log(dataUriFile);
+      const result = await cloudinary.uploader.upload(dataUriFile);
+      response.json(result);
+    } catch (error) {
+      console.log(error);
+      response.status(500).send(error.message);
+    }
+  }
+);
 
 server.use(middlewares);
 server.use('/api', router);
