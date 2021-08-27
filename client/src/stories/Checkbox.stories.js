b854fca65e39c055c02761099213bf85a8ac4c6a
Lenkly <lenk.lydia@gmail.com>
2021-08-27 16:51:55 +0200

    forcing stable babel-loader version

--- client/src/stories/Checkbox.stories.js
+++ client/src/stories/Checkbox.stories.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Checkbox from '../components/Checkbox';
+import Favourite from '../assets/Favourite';
 
 export default {
   title: 'Checkbox',
 };
 
-export const HeartCheckbox = () => {
-  <Checkbox label="Testi McTestface" />;
-};
+export const HeartCheckbox = () => (
+  <Checkbox
+    label="Testi McTestface"
+    icon={<Favourite />}
+    onFavouriteClick={() => console.log('click')}
+  />
+);
